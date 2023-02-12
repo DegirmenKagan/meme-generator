5ec330e78707884f2112686899ab426096e18a8d
Kağan Değirmen <kagandegirmen@gmail.com>
2023-02-12 23:57:14 +0300

    Header implementation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <h1 className="header--title">Meme Generator</h1>
+      <a className="header--github" href="https://github.com/DegirmenKagan">
+        Github
+      </a>
+    </header>
+  );
 }
