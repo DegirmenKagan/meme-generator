ea03bb763b2210409bf614eadd2fd204afcb13c0
Kağan Değirmen <kagandegirmen@gmail.com>
2023-02-14 22:43:09 +0300

    Meme > state refactoring, image css

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,19 +3,30 @@ import { useState } from "react";
 import memesData from "../data/memesData.js";
 
 export default function Meme() {
-  const [text1, setText1] = useState("Bu site");
-  const [text2, setText2] = useState("Bu site bi harika dostum");
+  // const [text1, setText1] = useState("Bu site");
+  // const [text2, setText2] = useState("Bu site bi harika dostum");
 
-  function handleText1Change(event) {
-    setText1(event.target.value);
-  }
-  function handleText2Change(event) {
-    setText2(event.target.value);
-  }
+  // function handleText1Change(event) {
+  //   setText1(event.target.value);
+  // }
+  // function handleText2Change(event) {
+  //   setText2(event.target.value);
+  // }
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "",
+  });
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
+
   function getMemeImage() {
-    const memesArray = memesData.data.memes;
+    const memesArray = allMemeImages.data.memes;
     const randomNum = Math.floor(Math.random() * memesArray.length);
-    console.log(memesArray[randomNum]);
+    const url = memesArray[randomNum].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
   }
   return (
     <main>
@@ -41,6 +52,7 @@ export default function Meme() {
           Get a new meme image 🖼
         </button>
       </div>
+      <img className="meme--image" src={meme.randomImage} />
     </main>
   );
 }
